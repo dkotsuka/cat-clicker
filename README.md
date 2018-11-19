26fcaf59092be686477acb3bd1b364548f95f382
Daniel Otsuka <dkotsuka@gmail.com>
2018-11-19 13:25:14 -0200

    feat: Edição de gatos
    Adicionada a funcionalidade de edição de nome, url da imagem e número de clicks em cada gato através de text inputs.

--- README.md
+++ README.md
@@ -2,17 +2,16 @@
 
 ## Sobre o projeto
 
-Projeto simples para treinar o Javascript e Jquery.
+Projeto simples para treinar a modularização de códigos em Javascript.
 Criado com diretrizes do curso de Web Front End da [Udacity](udacity.com)
 
 Para visualizar a página, [click aqui!](https://dkotsuka.github.io/cat-clicker/index.html)
 
 ## Habilidades
 
-Este projeto foi útil para:
-* treinar a sintaxe do Jquery;
-* treinar scripts dinâmicos de preenchimento de listas com o Javascript;
-* treinar CSS responsivo.
-
-Refatoramento 1:
-* Projeto reconstruído para modularizar o código, separando dados das views e do código manipulador.
+Este projeto foi útil para treinar:
+* a sintaxe do Jquery;
+* scripts dinâmicos de preenchimento de listas com o Javascript;
+* o CSS responsivo.
+* a escrita de código modularizado e implementação de novas funcionalidades a partir de novos módulos.
+* separação de dados da UI e uma codificação mais organizada.
--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ $(function() {
 		},
 		render: function() {
 			const cats = octopus.getCats();
+			$('#list-of-cats').text('');
 			cats.forEach( function(cat, index) {
 				const listItem = document.createElement('li');
 				const catName = document.createElement('h2');
@@ -47,6 +48,7 @@ $(function() {
 			const catName = document.createElement('h2');
 			const catImage = document.createElement('img');
 			const clickCounter = document.createElement('span');
+			const admButton = document.createElement('button');
 			container.setAttribute('id', 'selected-container');
 			catName.innerHTML = cat.name;
 			catImage.src = cat.image;
@@ -55,10 +57,66 @@ $(function() {
 			container.append(catImage);
 			container.append(clickCounter);
 			$('#cat-selected-container').append(container);
+			admButton.innerHTML = 'Edit cat';
+			admButton.classList.add('adm-button');
+			$('#cat-selected-container').append(admButton);
 
 			container.addEventListener('click', function(e) {
 				octopus.clickCount(index);
 			});
+
+			admButton.addEventListener('click', function(e) {
+				octopus.showEditArea(index);
+				admButton.style.display = 'none';
+			});
+		}
+	};
+
+	const editView = {
+		show: function(index) {
+			const cat = octopus.getCat(index);
+			const container = document.createElement('form');
+			const inputName = document.createElement('input');
+			const nameLabel = document.createElement('label');
+			inputName.setAttribute('value', cat.name);
+			nameLabel.innerHTML = 'Name:';
+			nameLabel.append(inputName);
+			const inputImage = document.createElement('input');
+			inputImage.setAttribute('value', cat.image);
+			const imageLabel = document.createElement('label');
+			imageLabel.innerHTML = 'Image URL:';
+			imageLabel.append(inputImage);
+			const inputClicks = document.createElement('input');
+			inputClicks.setAttribute('value', cat.clicks);
+			const clicksLabel = document.createElement('label');
+			clicksLabel.innerHTML = 'Clicks:';
+			clicksLabel.append(inputClicks);
+			const saveButton = document.createElement('button');
+			saveButton.classList.add('save-button');
+			saveButton.innerHTML = 'Save changes';
+			const cancelButton = document.createElement('button');
+			cancelButton.classList.add('cancel-button');
+			cancelButton.innerHTML = 'cancel';
+			container.append(nameLabel);
+			container.append(imageLabel);
+			container.append(clicksLabel);
+			container.append(saveButton);
+			container.append(cancelButton);
+			$('#cat-selected-container').append(container);
+
+			saveButton.addEventListener('click', function(e) {
+				const newCat = {name: inputName.value, clicks: inputClicks.value, image: inputImage.value};
+				editView.saveChanges(index, newCat);
+				e.preventDefault();
+			});
+
+			cancelButton.addEventListener('click', function(e) {
+				octopus.cancelEdit(index);
+				e.preventDefault();
+			});
+		},
+		saveChanges: function(index, cat) {
+			octopus.editCat(index,cat);
 		}
 	};
 
@@ -77,6 +135,19 @@ $(function() {
 			selectView.render(index);
 		},
 		listItemClick: function(index) {
+			selectView.render(index);
+		},
+		showEditArea: function(index) {
+			editView.show(index);
+		},
+		editCat: function(index, cat) {
+			model.cats[index].name = cat.name;
+			model.cats[index].clicks = cat.clicks;
+			model.cats[index].image = cat.image;
+			selectView.render(index);
+			listView.render();
+		},
+		cancelEdit: function(index) {
 			selectView.render(index);
 		}
 	};
